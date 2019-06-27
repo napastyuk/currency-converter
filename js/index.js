24d2f77ae3e2d574ade076249968b4f200a36c47
ilya <napastyuk@live.ru>
2019-06-28 01:05:27 +0300

    add css improvements

--- js/index.js
+++ js/index.js
@@ -1,71 +1,125 @@
-//1. сделать запрос на https://www.cbr-xml-daily.ru/daily.xml и получить xml
-//c https://www.cbr.ru/scripts/XML_daily.asp данные получить сложнее по нескольким причинам
-//1- у них не настроена CORS, поэтому запросы из браузера блочатся самим браузером,
-//2- документ у них отдается в кодировке windows-1251 , что тянет за собой библиотеку для перекодировки
-//3- формат отдаваемого документа xml, что в приципе решаемо, но на cbr-xml-daily есть возможность переключить на json
-//2. преобразовать данные из xml в js объект
-//3. cохранить его в servise worker
-//4. вывести ofline калькулятор валют
-
-// if (navigator.serviceWorker) {
-//     navigator.serviceWorker.register('/serviceworker.js');
-// }
+(function init(){
 
 const CBRF_COURSEAPI_URL = "https://www.cbr-xml-daily.ru/daily_json.js";
 let newRates = {}; //Объект со всем курсами
 const rootId = "currencyCalc";
-let currencyCount = "2";
+let currencyCount = "1";
 let defaultCurrencyCode = "RUB";
 let defaultСalculatedCurrency = "USD";
 
-function init() {
+checkStorage();
+
+function checkStorage() {
+    //перед отправкой запроса проверить что в сторадже уже не лежит сегодняший курс
+    if (localStorage.getItem('lastGettedRatesDate') == new Date().getDate()) {
+        // если в localStorage актуальные данные достаем курс локально
+        console.log('достаем курсы валют локально');
+        getFromLocal();
+        initView();
+    } else if (localStorage.getItem('lastGettedRatesDate') != new Date().getDate() ||
+        localStorage.getItem('lastGettedRatesDate') === null) {
+        //если localStorage пустой или просрочен обновляем курсы валют по сети
+        sendXMLHttpRequest(CBRF_COURSEAPI_URL);
+        console.log('обновляем курсы валют по сети');
+    };
+};    
+
+function sendXMLHttpRequest(url) {
+    console.log("отправляем запрос на " + url);
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+        // console.log("this.readyState " + this.readyState)
+        // console.log("this.status " + this.status)
+        if (this.readyState == 4 && this.status == 0) {
+            console.error('Ошибка CORS. Статус: ' + this.status);
+        } else if (this.readyState == 4 && this.status == 200) {
+            saveToLocal(xhr.responseText);
+            initView();
+        }
+    };
+    xhr.send();
+}
+
+function saveToLocal(response) {
+    localStorage.removeItem('lastGettedRatesDate');
+    localStorage.removeItem('lastGettedRates');
+    localStorage.setItem("lastGettedRates", response);
+    localStorage.setItem("lastGettedRatesDate", new Date().getDate());
+    newRates = JSON.parse(response);
+}
+
+function getFromLocal() {
+    newRates = JSON.parse(localStorage.getItem("lastGettedRates"));
+}
+
+function initView() {
     let targetEl = document.getElementById(rootId);
     if (!targetEl) {
         console.error("элемент с id currencyCalc не найден");
         return
-    } else
-        checkStorage()
-    generateCalcView(targetEl);
+    } else {
+        generateCalcView(targetEl);
+    }
 }
 
+//перед рисованием убедится что курсы валют уже прогрузились!!!!
 function generateCalcView() {
     // console.log(newRates.Valute.USD.Value * 50);   
     let rootEl = document.getElementById(rootId);
-    let formEl = document.createElement('form');
+
     for (let i = 1; i <= currencyCount; i++) {
-        addNewCurrency(formEl);
+        let divEl = document.createElement('div');
+        divEl.classList.add('row');
+        divEl.classList.add('currencyRow');
+        addNewCurrency(divEl);
+        rootEl.appendChild(divEl);
     }
-    addFinalCurrency(formEl);
-    rootEl.appendChild(formEl);
+    let lastdivEl = document.createElement('div');
+    lastdivEl.classList.add('row');
+    lastdivEl.classList.add('currencyRow');
+    addFinalCurrency(lastdivEl);
+    rootEl.appendChild(lastdivEl);
 }
 
 function addNewCurrency(containerEl) {
-    containerEl.appendChild(generateCurrencySelect(defaultСalculatedCurrency));
     let inputEl = document.createElement('input');
     inputEl.setAttribute('type', 'number');
     inputEl.dataset.curCode = defaultСalculatedCurrency;
     inputEl.value = "0";
     inputEl.addEventListener('input', recalculateInputValues);
-    containerEl.appendChild(inputEl);
-    let brEl = document.createElement('br');
-    containerEl.appendChild(brEl);
+    let leftdivEl = document.createElement('div');
+    leftdivEl.classList.add('col-2');
+    leftdivEl.appendChild(inputEl);
+    containerEl.appendChild(leftdivEl);
+    let rightdivEl = document.createElement('div');
+    rightdivEl.classList.add('col-6');
+    containerEl.appendChild(rightdivEl);
+    rightdivEl.appendChild(generateCurrencySelect(defaultСalculatedCurrency));
 }
 
 function addFinalCurrency(containerEl) {
     //последняя валюта в списке
-    let labelEl = document.createElement('span');
-    labelEl.innerText = "Российский рубль";
-    containerEl.appendChild(labelEl);
     let inputEl = document.createElement('input');
     inputEl.setAttribute('type', 'number');
     inputEl.setAttribute('id', 'finalCurrency');
     inputEl.dataset.curCode = defaultCurrencyCode;
     inputEl.value = "0";
     inputEl.addEventListener('input', recalculateInputValues);
-    containerEl.appendChild(inputEl);
+    let leftdivEl = document.createElement('div');
+    leftdivEl.classList.add('col-2');
+    leftdivEl.appendChild(inputEl);
+    containerEl.appendChild(leftdivEl);
+    let rightdivEl = document.createElement('div');
+    rightdivEl.classList.add('col-6');
+    containerEl.appendChild(rightdivEl);
+    let labelEl = document.createElement('span');
+    labelEl.innerText = "Российский рубль";
+    rightdivEl.appendChild(labelEl);
 }
 
 function generateCurrencySelect(selectedValue) {
+    // debugger;
     let selectEl = document.createElement('select');
     for (let key in newRates.Valute) {
         let optionEl = new Option(newRates.Valute[key].Name, newRates.Valute[key].CharCode);
@@ -91,8 +145,8 @@ function recalculateInputValues() {
     if (chaingedCurrencyValue == 0 || isNaN(chaingedCurrencyValue)) {
         console.log('значение равно 0');
         return;
-    }   
-    
+    }
+
     //так как у нас курсы валют относительно рубля, отдельно считаем изменение рубля и остальных валют
     if (chaingedCurrencyCode == defaultCurrencyCode) {
         //пройдемся по всем валютам пересчитаем значение в них опираясь на значение в рублёвом инпуте
@@ -120,52 +174,14 @@ function recalculateAllCurrency() {
         //ищем курс валют от i-го инпута
         let courseOfСurrentInput = newRates.Valute[allCurrencyInputs[i].dataset.curCode].Value;
         allCurrencyInputs[i].value = defaultCurrencyValue / courseOfСurrentInput;
-    }
-}
-
-function checkStorage() {
-    //перед отправкой проверить что в сторадже уже не лежит сегодняший курс
-    if (localStorage.getItem('lastGettedRatesDate') == new Date().getDate()) {
-        // если в localStorage актуальные данные достаем курс локально
-        console.log('достаем курсы валют локально');
-        getFromLocal();
-    } else if (localStorage.getItem('lastGettedRatesDate') != new Date().getDate() ||
-        localStorage.getItem('lastGettedRatesDate') === null) {
-        //если localStorage пустой или просрочен обновляем курсы валют по сети
-        sendXMLHttpRequest(CBRF_COURSEAPI_URL);
-        console.log('обновляем курсы валют по сети');
     };
-}
+};
 
-function sendXMLHttpRequest(url) {
-    console.log("отправляем запрос на " + url);
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-        // console.log("this.readyState " + this.readyState)
-        // console.log("this.status " + this.status)
-        if (this.readyState == 4 && this.status == 0) {
-            console.error('Ошибка CORS. Статус: ' + this.status);
-        } else if (this.readyState == 4 && this.status == 200) {
-            saveToLocal(xhr.responseText);
-        }
-    };
-    xhr.send();
-}
+}());
 
-function saveToLocal(response) {
-    localStorage.removeItem('lastGettedRatesDate');
-    localStorage.removeItem('lastGettedRates');
-    localStorage.setItem("lastGettedRates", response);
-    localStorage.setItem("lastGettedRatesDate", new Date().getDate());
-    newRates = JSON.parse(response);
-}
 
-function getFromLocal() {
-    newRates = JSON.parse(localStorage.getItem("lastGettedRates"));
-}
 
-init();
+
 
 
 
